0649245652ce38db81512494cbd8366cc78466bf
r-meynet <remy.meynet@outlook.fr>
2025-11-06 23:11:35 +0100

    Factorisation du code des fx d'affichage des covoiturages

--- js/script.js
+++ js/script.js
@@ -18,77 +18,6 @@ function createStars() {
     });
 }
 
-// Fonction qui va afficher les trajets
-function affichageTrajets(trips) {
-    // Variables pour l'affichage sur la page
-    const container = document.getElementById("affichage-trajets");
-    const affichageResultat = document.getElementById("nombreResultats");
-    let nombreTrajets = 0;
-
-    // On vide le contenu existant
-    container.innerHTML = "";
-
-    // Variables relatives à l'affichage d'un trajet eco
-    let classEcoCard = "";
-    let classEcoBtn = "";
-    let classEcoLogo = "";
-
-    // Boucle parmis tous les trajets
-    trips.forEach((trip) => {
-        // Gestion de l'affichage d'un trajet écologique
-        if (trip.eco) {
-            classEcoCard = "card mb-4 border-3 border-primary shadow";
-            classEcoBtn = "btn btn-primary";
-            classEcoLogo = "fs-5 text-primary";
-        } else {
-            classEcoCard = "card mb-4 border-0 shadow";
-            classEcoBtn = "btn btn-outline-secondary";
-            classEcoLogo = "d-none";
-        }
-
-        // Création de la carte trajet
-        const cardTrajet = `
-        <div class="${classEcoCard}">
-        <div class="row row-cols-2 row-cols-lg-4 gx-3 align-items-center">
-        <div class="col d-flex flex-column order-lg-1 px-4 py-2">
-        <div class="d-flex flex-row">
-        <img src="${trip.photo}" class="profile-picture" alt="Photo de profil de ${trip.pseudo}" />
-        <h6 class="fs-5 ps-2">${trip.pseudo}</h6>
-        </div>
-        <div class="text-primary rating" data-rating="${trip.note}"></div>
-        </div>
-        <div class="col order-lg-2 ps-3 py-2">
-        <div>🚗 ${trip.ville_depart} → ${trip.ville_arrivee}</div>
-        <div>
-        <div>📅 ${trip.date}</div>
-        <div>⏰ ${trip.heure_depart} - ${trip.heure_arrivee}</div>
-        </div>
-        </div>
-        <div class="col row row-cols-2 row-cols-lg-1 order-lg-3 ps-3 py-2 mb-auto my-lg-auto text-center fs-5">
-        <div class="col pe-0">${trip.credit} <i class="bi bi-coin"></i></div>
-        <div class="col"><i class="bi bi-people-fill d-lg-none"></i>${trip.places_disponibles} <span class="d-none d-lg-inline">places disponibles</span></div>
-        </div>
-        <div class="col order-lg-4 px-4 py-2 text-center my-auto">
-        <a href="/details" class="${classEcoBtn}">+ détails</a>
-        <div class="${classEcoLogo}">eco <i class="bi bi-leaf-fill"></i></div>
-        </div>
-        </div>
-        </div>
-        `;
-
-        container.innerHTML += cardTrajet;
-        nombreTrajets += 1;
-    });
-
-    if (nombreTrajets <= 1) {
-        affichageResultat.innerHTML = `${nombreTrajets} résultat`;
-    } else {
-        affichageResultat.innerHTML = `${nombreTrajets} résultats`;
-    }
-
-    createStars();
-}
-
 // Requête auprès de la base de données test en json
 let tripCache = [];
 
@@ -97,3 +26,94 @@ fetch("../data/data.json")
     .then((data) => {
         tripCache = data.trips;
     });
+
+// Fonction qui va afficher les trajets
+function affichageTrajets(trips) {
+    // Variables pour l'affichage sur la page
+    const container = document.getElementById("affichage-trajets");
+    const htmlResultats = document.getElementById("nombreResultats");
+    let nombreTrajets = 0;
+
+    // On vide le contenu existant
+    container.innerHTML = "";
+
+    // Affichage si trips contient des éléments
+    if (trips.length > 0) {
+        // Boucle parmis tous les trajets
+        trips.forEach((trip) => {
+            // Création de la carte trajet si des places sont toujours disponibles
+            if (trip.places_disponibles > 0) {
+                creationCarteTrajet(trip, container);
+                nombreTrajets += 1;
+            }
+        });
+
+        // Affichage des étoiles d'avis
+        createStars();
+    } else {
+        container.innerHTML =
+            '<h4 class="text-center">Aucun trajet ne correspond aux critères de recherche</h4>';
+    }
+
+    // Afficher le nombre de résultat en haut du containerr
+    afficherNombreResultat(nombreTrajets, htmlResultats);
+}
+
+// Fonction pour créer une carte trajet
+function creationCarteTrajet(trip, destination) {
+    // Gestion de l'affichage d'un trajet écologique
+    let classEcoCard = "";
+    let classEcoBtn = "";
+    let classEcoLogo = "";
+
+    if (trip.eco) {
+        classEcoCard = "card mb-4 border-3 border-primary shadow";
+        classEcoBtn = "btn btn-primary";
+        classEcoLogo = "fs-5 text-primary";
+    } else {
+        classEcoCard = "card mb-4 border-0 shadow";
+        classEcoBtn = "btn btn-outline-secondary";
+        classEcoLogo = "d-none";
+    }
+
+    // Contenu de la carte
+    const cardTrajet = `
+    <div class="${classEcoCard}">
+    <div class="row row-cols-2 row-cols-lg-4 gx-3 align-items-center">
+    <div class="col d-flex flex-column order-lg-1 px-4 py-2">
+    <div class="d-flex flex-row">
+    <img src="${trip.photo}" class="profile-picture" alt="Photo de profil de ${trip.pseudo}" />
+    <h6 class="fs-5 ps-2">${trip.pseudo}</h6>
+    </div>
+    <div class="text-primary rating" data-rating="${trip.note}"></div>
+    </div>
+    <div class="col order-lg-2 ps-3 py-2">
+    <div>🚗 ${trip.ville_depart} → ${trip.ville_arrivee}</div>
+    <div>
+    <div>📅 ${trip.date}</div>
+    <div>⏰ ${trip.heure_depart} - ${trip.heure_arrivee}</div>
+    </div>
+    </div>
+    <div class="col row row-cols-2 row-cols-lg-1 order-lg-3 ps-3 py-2 mb-auto my-lg-auto text-center fs-5">
+    <div class="col pe-0">${trip.credit} <i class="bi bi-coin"></i></div>
+    <div class="col"><i class="bi bi-people-fill d-lg-none"></i>${trip.places_disponibles} <span class="d-none d-lg-inline">places disponibles</span></div>
+    </div>
+    <div class="col order-lg-4 px-4 py-2 text-center my-auto">
+    <a href="/details" class="${classEcoBtn}">+ détails</a>
+    <div class="${classEcoLogo}">eco <i class="bi bi-leaf-fill"></i></div>
+    </div>
+    </div>
+    </div>
+    `;
+
+    destination.innerHTML += cardTrajet;
+}
+
+// Fonction d'affichage du nombre de résultats
+function afficherNombreResultat(nombreResultats, destination) {
+    if (nombreResultats <= 1) {
+        destination.innerHTML = `${nombreResultats} résultat`;
+    } else {
+        destination.innerHTML = `${nombreResultats} résultats`;
+    }
+}
