8abdb471a238fc74bafcde990b2bfd9cbc17c4de
r-meynet <remy.meynet@outlook.fr>
2025-11-11 22:45:30 +0100

    Ajout logique de connexion signin.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 // Requête auprès de la base de données test en json
 let tripBrut = [];
+let usersArray = [];
 
 fetch("../data/data.json")
     .then((reponse) => reponse.json())
     .then((data) => {
         tripBrut = data.trips;
+        usersArray = data.users;
     });
 
 // Création d'une fonction pour récupérer la moyenne des avis et la transformer en étoiles
@@ -103,3 +105,63 @@ function isEmpty(idForm) {
         return false;
     }
 }
+
+// START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ COOKIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ COOKIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END
+//
+//
+//
+//
+//
+// START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CONNEXION + ROLES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const nameCookieToken = "accesstoken";
+const nameCookieRole = "role";
+
+function setToken(token) {
+    setCookie(nameCookieToken, token, 7);
+}
+
+function getToken() {
+    return getCookie(nameCookieToken);
+}
+
+function isConnected() {
+    if (getToken() == null || getToken() == undefined) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CONNEXION + ROLES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END
